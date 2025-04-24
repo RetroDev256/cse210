433d6d5584f4a762268bb54de7171f8d2376577e
Eric Petersen <ericpetersen3.14@gmail.com>
2025-04-24 13:40:58 -0600

    start main project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Input a number: ");
+string inp = Console.ReadLine();
+int inp_int = int.Parse(inp);
+
+string prefix = $"The number: {inp} is ";
+string suffix;
+if (inp_int > 0) {
+    suffix = "more than zero.";
+} else if (inp_int < 0) {
+    suffix = "less than zero.";
+} else {
+    suffix = "equal to zero.";
+}
+
+Console.WriteLine($"{prefix}{suffix}");
