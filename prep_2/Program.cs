593dca98572af0134c7e99bb29d86434f90c2ad0
Eric Petersen <ericpetersen3.14@gmail.com>
2025-04-26 16:50:16 -0600

    prep 1 and 2 projects completed

---
+++ prep_2/Program.cs
@@ -0,0 +1,60 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your grade percentage: ");
+        int grade;
+
+        while (true)
+        {
+            string input = Console.ReadLine() ?? "0";
+
+            try
+            {
+                grade = int.Parse(input);
+                if (grade >= 0 && grade <= 100)
+                {
+                    break;
+                }
+            }
+            catch { }
+
+            Console.Write("Invalid input. Try again: ");
+        }
+
+
+        char letter;
+
+        if (grade >= 90)
+        {
+            letter = 'A';
+        }
+        else if (grade >= 80)
+        {
+            letter = 'B';
+        }
+        else if (grade >= 70)
+        {
+            letter = 'C';
+        }
+        else if (grade >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        Console.WriteLine($"You have a {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed! Great job.");
+        }
+        else
+        {
+            Console.WriteLine("You failed! Try harder next time.");
+        }
+    }
+}
