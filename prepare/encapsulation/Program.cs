5242a05a61d6036529db0a431dea730b26696fc1
Eric Petersen <ericpetersen3.14@gmail.com>
2025-05-22 15:56:35 -0600

    prepare assignment

---
+++ prepare/encapsulation/Program.cs
@@ -0,0 +1,64 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Fraction pi_approx = new Fraction(333, 106);
+        string fract = pi_approx.GetFractionString();
+        double value = pi_approx.GetDecimalValue();
+        Console.WriteLine($"{fract}: {value}");
+    }
+}
+
+class Fraction
+{
+    private int _top;
+    private int _bot;
+
+    public Fraction()
+    {
+        this._top = 0;
+        this._bot = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        this._top = wholeNumber;
+        this._bot = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        this._top = top;
+        this._bot = bottom;
+    }
+
+    public int GetTop()
+    {
+        return this._top;
+    }
+
+    public void SetTop(int top)
+    {
+        this._top = top;
+    }
+
+    public int GetBottom()
+    {
+        return this._bot;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        this._bot = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{this._top}/{this._bot}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)this._top / this._bot;
+    }
+}
--- prove/Develop02/Picker.cs
+++ prove/Develop02/Picker.cs
@@ -2,32 +2,32 @@
 // ask the user for each journal entry.
 
 class Picker
-    {
-        static Random _rng = new Random();
-        static List<string> _questions = [
-            "Did you meet anyone new today?",
-            "What has been the most exciting part of today?",
-            "What are you most proud of for today?",
-            "What was the most interesting interaction you had today?",
-            "If you could do one thing over again, what would it be?",
-            "What surprised you the most today?",
-            "What did you learn today, either about yourself or the world?",
-            "How did you take care of yourself today?",
-            "Was there a moment you felt truly at peace?",
-            "Did you face any challenges or obstacles today? How did you handle them?",
-            "Who made a positive impact on your day?",
-            "What is one thing you are grateful for today?",
-            "How did you express kindness today, either to yourself or to someone else?",
-            "What did you avoid doing today, and why?",
-            "Was there a moment you wish you had handled differently?",
-            "What made you smile or laugh today?",
-            "What is one thing you would like to remember about today?",
-            "Did anything today push you outside your comfort zone?"
-        ];
+{
+    static Random _rng = new Random();
+    static List<string> _questions = [
+        "Did you meet anyone new today?",
+        "What has been the most exciting part of today?",
+        "What are you most proud of for today?",
+        "What was the most interesting interaction you had today?",
+        "If you could do one thing over again, what would it be?",
+        "What surprised you the most today?",
+        "What did you learn today, either about yourself or the world?",
+        "How did you take care of yourself today?",
+        "Was there a moment you felt truly at peace?",
+        "Did you face any challenges or obstacles today? How did you handle them?",
+        "Who made a positive impact on your day?",
+        "What is one thing you are grateful for today?",
+        "How did you express kindness today, either to yourself or to someone else?",
+        "What did you avoid doing today, and why?",
+        "Was there a moment you wish you had handled differently?",
+        "What made you smile or laugh today?",
+        "What is one thing you would like to remember about today?",
+        "Did anything today push you outside your comfort zone?"
+    ];
 
-        public static string GetRandom()
-        {
-            int index = _rng.Next(_questions.Count);
-            return _questions[index];
-        }
+    public static string GetRandom()
+    {
+        int index = _rng.Next(_questions.Count);
+        return _questions[index];
     }
+}
